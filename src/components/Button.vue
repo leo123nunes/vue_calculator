30df8509a7474f6e9620ba793e4f6274f4728368
leo123nunes <ti.userleo@hotmail.com>
2020-12-30 09:37:19 -0300

    Adding the calculator's buttons

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,15 +1,57 @@
 <template>
-  <button class="button">
-      0
+  <button class="button" :class='{operation, double, triple}'>
+    {{label}}
   </button>
 </template>
 
 <script>
 export default {
-
+  props: {
+    label: String,
+    operation: Boolean,
+    double: Boolean,
+    triple: Boolean
+  }
 }
 </script>
 
 <style>
+button.button{
+  background-color: rgb(235, 235, 235);
+  padding: 0px;
+  border: 1px solid #bdbdbd;
+  border-left: none;
+  border-top: none;
+  color: rgb(0, 0, 0);
+  font-size: 20px;
+}
+
+button.button:hover{
+  background-color: #bdbdbd;
+  text-decoration: none;
+  outline: none;
+}
+
+button.double{
+  grid-column: span 2;
+}
+
+button.triple{
+  grid-column: span 3;
+}
+
+button.operation{
+  background-color: rgb(255, 100, 28);
+  color: #fff;
+}
+
+button.operation:hover{
+  background-color: rgb(175, 71, 22);
+}
+
+button.button:active{
+  outline: none;
+  text-decoration: none;
+}
 
 </style>
--- src/main/Calculator.vue
+++ src/main/Calculator.vue
@@ -1,10 +1,22 @@
 <template>
     <div class="calculator">
         <Display></Display>
-        <Button></Button>
-        <Button></Button>
-        <Button></Button>
-        <Button></Button>
+        <Button label="AC" triple></Button>
+        <Button label="/" operation></Button>
+        <Button label="7"></Button>
+        <Button label="8"></Button>
+        <Button label="9"></Button>
+        <Button label="*" operation></Button>
+        <Button label="4"></Button>
+        <Button label="5"></Button>
+        <Button label="6"></Button>
+        <Button label="-" operation></Button>
+        <Button label="1"></Button>
+        <Button label="2"></Button>
+        <Button label="3"></Button>
+        <Button label="+" operation></Button>
+        <Button label="0" double></Button>
+        <Button label="."></Button>
     </div>
   
 </template>
@@ -21,12 +33,11 @@ export default {
 
 <style>
 .calculator{
-    width: 300px;
+    width: 250px;
     height: 300px;
     display: grid;
     border-radius: 5px;
     overflow: hidden;
-    background-color: rgb(245, 108, 108);
     grid-template-columns: repeat(4, 25%);
     grid-template-rows: 1fr 48px 48px 48px 48px 48px;
 }
